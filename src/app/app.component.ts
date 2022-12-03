c8fbb74686cffd4153098b21184529e776ff6ba4
andreichisilev <andreichisilev@gmail.com>
2022-12-03 16:56:50 +0200

    upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'temaLab2';
+  content = ["a","b","c","d","e","f"];
+}
---
+++ src/app/content/content.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.scss']
+})
+export class ContentComponent {
+
+  cards = ["a","b","c","d","e","f"];
+}
